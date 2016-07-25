575725dead44f28e5ce047d3a0275d10564290ca
Todd D. Vance <tdvance@gmail.com>
2016-07-25 12:20:29 -0400

    New Unreal Editor 4 Project

---
+++ SpaceWar/Source/SpaceWar/SpaceWarGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SpaceWarGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEWAR_API ASpaceWarGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
