879985ea66d5ba0a33ad888c4517e29f401cf36a
Todd D. Vance <tdvance@gmail.com>
2016-08-17 10:55:39 -0400

    stuck on a bug

--- SpaceWar/Source/SpaceWar/Private/ShipPlayerController.cpp
+++ SpaceWar/Source/SpaceWar/Private/ShipPlayerController.cpp
@@ -15,12 +15,34 @@ void AShipPlayerController::BeginPlay() {
 
 void AShipPlayerController::Tick(float DeltaSeconds) {
 	Super::Tick(DeltaSeconds);
-	AimTowardCrosshair();
+	AimTowardCrosshair(DeltaSeconds);
 }
 
-void AShipPlayerController::AimTowardCrosshair() {
+void AShipPlayerController::AimTowardCrosshair(float DeltaSeconds) {
 	if (!GetControlledShip()) {
 		return;
 	}
+
+	FVector AimDirectionVector;
+	//find the crosshair position
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	FVector2D ScreenLocation = FVector2D(ViewportSizeX*CrosshairXLocation, ViewportSizeY*CrosshairYLocation);
+	FVector CameraLocation;
+	DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, CameraLocation, AimDirectionVector);
+	FRotator AimDirection = AimDirectionVector.Rotation();
+	FRotator CurrentDirection = GetControlledShip()->GetActorForwardVector().Rotation();
+	FRotator DeltaRotation = AimDirection - CurrentDirection;
+	if (FMath::Abs(DeltaRotation.Yaw) >= 180) {
+		DeltaRotation.Yaw = -DeltaRotation.Yaw;
+	}
+	float mamount = DeltaSeconds*MaxDegPerSec;
+	DeltaRotation.Yaw = FMath::Clamp(DeltaRotation.Yaw, -mamount, mamount);
+	DeltaRotation.Pitch = FMath::Clamp(DeltaRotation.Pitch, -mamount, mamount);
+	DeltaRotation.Roll = FMath::Clamp(DeltaRotation.Roll, -mamount, mamount);
+
+	GetControlledShip()->AddActorWorldRotation(DeltaRotation);
+	UE_LOG(LogTemp, Warning, TEXT("Ship: %s  AimDirection: %s  CurrentDirection: %s  Diff: %s"), *GetControlledShip()->GetName(), *AimDirection.ToString(), *CurrentDirection.ToString(), *DeltaRotation.ToString());
+
 
 }
--- SpaceWar/Source/SpaceWar/Public/ShipPlayerController.h
+++ SpaceWar/Source/SpaceWar/Public/ShipPlayerController.h
@@ -16,12 +16,21 @@ class SPACEWAR_API AShipPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 private:
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.333;
+
+	UPROPERTY(EditAnywhere)
+	float MaxDegPerSec = 5.0;
+
 	AShip* GetControlledShip() const;
 	
 	void BeginPlay() override;
 
 	void Tick(float DeltaSeconds) override;
 
-	void AimTowardCrosshair();
+	void AimTowardCrosshair(float DeltaSeconds);
 
 };
